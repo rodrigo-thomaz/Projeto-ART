df7f6c4bce0e3a2e1a5d167343e758da8b6ac2d6
rthomaz <rodrigothomaz@msn.com>
2017-09-17 16:04:21 -0300

    ok

---
+++ souces/ART.Domotica.WebPages/app/controllers/dashboardController.js
@@ -0,0 +1,14 @@
+﻿'use strict';
+app.controller('dashboardController', ['$scope', 'dashboardService', function ($scope, dashboardService) {
+
+    $scope.orders = [];
+
+    dashboardService.getDashboards().then(function (results) {
+
+        $scope.dashboards = results.data;
+
+    }, function (error) {
+        alert(error.data.message);
+    });
+
+}]);
