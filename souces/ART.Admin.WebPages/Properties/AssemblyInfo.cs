6edd8d377aa452fc28a2891b03114058774c8530
rthomaz <rodrigothomaz@msn.com>
2017-09-17 17:12:21 -0300

    ok

--- souces/ART.Admin.WebPages/Properties/AssemblyInfo.cs
+++ souces/ART.Admin.WebPages/Properties/AssemblyInfo.cs
@@ -4,11 +4,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("ART.Admin.WebPages")]
+[assembly: AssemblyTitle("ART.Admin.Presentation.WebPages")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("ART.Admin.WebPages")]
+[assembly: AssemblyProduct("ART.Admin.Presentation.WebPages")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
