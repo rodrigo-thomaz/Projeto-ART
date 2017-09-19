ab3dd07af000261b29a5ede5a4f034e898edb97a
rthomaz <rodrigothomaz@msn.com>
2017-09-19 12:24:49 -0300

    ok

--- souces/ART.Domotica.Presentation.WebPages/src/js/controllers/termometroController.js
+++ souces/ART.Domotica.Presentation.WebPages/src/js/controllers/termometroController.js
@@ -73,7 +73,7 @@ app.controller('termometroController', ['$scope', 'termometroService', function 
         }
 
         $scope.d4 = $scope.getRandomData();
-    }    
+    }
 
     function signalRSample() {
 
@@ -101,10 +101,66 @@ app.controller('termometroController', ['$scope', 'termometroService', function 
         }());
     }
 
+    function socketioSample() {
+
+        var socket = io.connect("http://localhost:3000");
+        var ready = false;
+        
+        $("#submit").submit(function (e) {
+            e.preventDefault();
+            $("#nick").fadeOut();
+            $("#chat").fadeIn();
+            var name = $("#nickname").val();
+            var time = new Date();
+            $("#name").html(name);
+            $("#time").html('First login: ' + time.getHours() + ':' + time.getMinutes());
+
+            ready = true;
+            socket.emit("join", name);
+        });
+
+        socket.on("update", function (msg) {
+            if (ready) {
+                $('.chat').append('<li class="info">' + msg + '</li>')
+            }
+        });
+
+        $("#textarea").keypress(function (e) {
+            if (e.which == 13) {
+                var text = $("#textarea").val();
+                $("#textarea").val('');
+                var time = new Date();
+                $(".chat").append('<li class="self"><div class="msg"><span>'
+                    + $("#nickname").val() + ':</span>    <p>' + text + '</p><time>' +
+                    time.getHours() + ':' + time.getMinutes() + '</time></div></li>');
+                socket.emit("send", text);
+            }
+        });
+
+        socket.on("chat", function (client, msg) {
+            if (ready) {
+                var time = new Date();
+                $(".chat").append('<li class="other"><div class="msg"><span>' +
+                    client + ':</span><p>' + msg + '</p><time>' + time.getHours() + ':' +
+                    time.getMinutes() + '</time></div></li>');
+            }
+        });
+
+        $scope.medicoesTemp = [];
+
+        socket.on("temp", function (client, data) {
+            $scope.medicoesTemp.push(data);
+            console.log(data);
+        });
+
+    }
+
     serviceSample();
 
     templateSample();
 
     signalRSample();
 
- }]);
+    socketioSample();
+
+}]);
