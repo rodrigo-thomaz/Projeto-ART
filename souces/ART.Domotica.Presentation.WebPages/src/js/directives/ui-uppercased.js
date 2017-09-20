22211594eae9bdfdb9c379407dec1901b59f00b0
rthomaz <rodrigothomaz@msn.com>
2017-09-19 23:11:46 -0300

    ok

---
+++ souces/ART.Domotica.Presentation.WebPages/src/js/directives/ui-uppercased.js
@@ -0,0 +1,12 @@
+﻿angular.module('app')
+.directive('uppercased', function () {
+    return {
+        require: 'ngModel',
+        link: function (scope, element, attrs, modelCtrl) {
+            modelCtrl.$parsers.push(function (input) {
+                return input ? input.toUpperCase() : "";
+            });
+            element.css("text-transform", "uppercase");
+        }
+    };
+})
