4a246d237cb825c9a4afebe93c8977090a508c04
rthomaz <rodrigothomaz@msn.com>
2017-09-20 10:40:21 -0300

    ok

---
+++ souces/devices/Termometro/Termometro.ino
@@ -0,0 +1,15 @@
+/*
+ Name:		Termometro.ino
+ Created:	20/09/2017 10:39:31
+ Author:	rodrigo
+*/
+
+// the setup function runs once when you press reset or power the board
+void setup() {
+
+}
+
+// the loop function runs over and over again until power down or reset
+void loop() {
+  
+}
