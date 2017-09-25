d920cf3f9a82d610108e0648d0c11ba646a3c94b
rthomaz <rodrigothomaz@msn.com>
2017-09-25 18:51:17 -0300

    ok

--- souces/ART.Seguranca.Presentation.WebPages/app/controllers/refreshController.js
+++ souces/ART.Seguranca.Presentation.WebPages/app/controllers/refreshController.js
@@ -1,5 +1,5 @@
 ﻿'use strict';
-app.controller('refreshController', ['$scope', '$location', 'authService', function ($scope, $location, authService) {
+app.controller('refreshController', ['$scope', '$location', '$timeout', 'authService', function ($scope, $location, $timeout, authService) {
 
     $scope.authentication = authService.authentication;
     $scope.tokenRefreshed = false;
@@ -8,8 +8,21 @@ app.controller('refreshController', ['$scope', '$location', 'authService', funct
     $scope.refreshToken = function () {
 
         authService.refreshToken().then(function (response) {
+
             $scope.tokenRefreshed = true;
             $scope.tokenResponse = response;
+
+            var returnurl = $location.search().returnurl;
+            if (returnurl) {
+                // Workarround de 2s para esperar a autenticação replicar 
+                // nas applicações do IIS
+                $timeout(function () {
+                    window.location = returnurl;
+                }, 2000);
+            }
+            else {
+                $location.path('/orders');
+            }
         },
          function (err) {
              $location.path('/login');
