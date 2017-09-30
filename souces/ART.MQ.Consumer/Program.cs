6bdc634a42623874923fa16b0b28c63948884918
rthomaz <rodrigothomaz@msn.com>
2017-09-30 16:33:11 -0300

    ok

--- souces/ART.MQ.Consumer/Program.cs
+++ souces/ART.MQ.Consumer/Program.cs
@@ -1,10 +1,7 @@
-﻿using ART.MQ.Common.QueueNames;
-using ART.MQ.Consumer.Consumers.DSFamilyTempSensorConsumers;
-using ART.MQ.Consumer.Modules;
+﻿using ART.MQ.Consumer.Modules;
 using Autofac;
 using MassTransit;
 using System;
-using System.Configuration;
 
 namespace ART.MQ.Consumer
 {
@@ -17,50 +14,22 @@ namespace ART.MQ.Consumer
 
         private static IContainer CreateContainer()
         {
-            IContainer container = null;
-
             var builder = new ContainerBuilder();
 
             builder.RegisterType<ARTDbContext>().InstancePerLifetimeScope();
             
             builder.RegisterModule<DSFamilyTempSensorModule>();
-            
-            builder.Register(context =>
-            {
-                var busControl = Bus.Factory.CreateUsingRabbitMq(rabbit =>
-                {
-                    var hostName = ConfigurationManager.AppSettings["RabbitMQHostName"];
-                    var virtualHostName = ConfigurationManager.AppSettings["RabbitMQVirtualHostName"];
-                    var username = ConfigurationManager.AppSettings["RabbitMQUsername"];
-                    var password = ConfigurationManager.AppSettings["RabbitMQPassword"];
+            builder.RegisterModule<BusModule>();
 
-                    var host = rabbit.Host(hostName, virtualHostName, settings =>
-                    {
-                        settings.Username(username);
-                        settings.Password(password);
-                    });
+            IContainer container = builder.Build();
 
-                    rabbit.ReceiveEndpoint(host, DSFamilyTempSensorQueueNames.DSFamilyTempSensorSetResolutionQueue, e =>
-                    {
-                        e.Consumer<DSFamilyTempSensorSetResolutionConsumer>(context);
-                    });
+            var busControl = container.Resolve<IBusControl>();
 
-                });
-
-                return busControl;
-            })
-                .SingleInstance()
-                .As<IBusControl>()
-                .As<IBus>();
-
-            container = builder.Build();
-
-            var bc = container.Resolve<IBusControl>();
-            bc.Start();
+            busControl.Start();
 
             Console.ReadKey();
 
-            bc.Stop();
+            busControl.Stop();
 
             return container;
         }
