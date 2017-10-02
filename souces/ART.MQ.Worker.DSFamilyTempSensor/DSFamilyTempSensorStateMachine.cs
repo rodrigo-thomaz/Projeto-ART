663293783a5955545078c619d84c3423aa4acd8f
rthomaz <rodrigothomaz@msn.com>
2017-10-02 02:22:17 -0300

    ok

--- souces/ART.MQ.Worker.DSFamilyTempSensor/DSFamilyTempSensorStateMachine.cs
+++ souces/ART.MQ.Worker.DSFamilyTempSensor/DSFamilyTempSensorStateMachine.cs
@@ -35,7 +35,7 @@ namespace ART.MQ.Worker.DSFamilyTempSensor
             // missed data.
             DuringAny(
                 When(SetResolution)
-                    .Then(context => 
+                    .Then(context =>
                     {
                         context.Instance.CreateTime = context.Data.Timestamp;
                     }),
