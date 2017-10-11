2c8e839ac28d7202096ed7016246849257c90cb3
rthomaz <rodrigothomaz@msn.com>
2017-10-11 18:23:16 -0300

    ok

---
+++ souces/ART.Infra.CrossCutting.Logging/ConsoleAppender.cs
@@ -0,0 +1,50 @@
+﻿using log4net.Appender;
+using log4net.Core;
+using log4net.Layout;
+
+namespace ART.Infra.CrossCutting.Logging
+{
+    public class ConsoleAppender : ColoredConsoleAppender
+    {
+        public ConsoleAppender()
+        {
+            Initialize();
+        }
+
+        private void Initialize()
+        {
+            AddMapping(new LevelColors
+            {
+                Level = Level.Debug,
+                ForeColor = Colors.White,
+            });
+
+            AddMapping(new LevelColors
+            {
+                Level = Level.Info,
+                ForeColor = Colors.Blue,
+            });
+
+            AddMapping(new LevelColors
+            {
+                Level = Level.Warn,
+                ForeColor = Colors.Yellow,
+            });
+
+            AddMapping(new LevelColors
+            {
+                Level = Level.Error,
+                ForeColor = Colors.Red,                
+            });
+
+            AddMapping(new LevelColors
+            {
+                Level = Level.Fatal,
+                ForeColor = Colors.White,
+                BackColor = Colors.Red | Colors.HighIntensity,
+            });
+
+            Layout = new PatternLayout("[%-5level][%property{callerNamespace}.%property{callerName}.%property{callerMethod}]%message %exception [%property{NDC}] %newline");
+        }
+    }
+}
