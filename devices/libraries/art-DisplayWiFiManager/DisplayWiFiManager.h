769276e935a6d0feceef611f80dc87e8b88a8d65
rthomaz <rodrigothomaz@msn.com>
2017-11-08 02:21:22 -0200

    ok

--- devices/libraries/art-DisplayWiFiManager/DisplayWiFiManager.h
+++ devices/libraries/art-DisplayWiFiManager/DisplayWiFiManager.h
@@ -15,34 +15,34 @@ public:
 	DisplayWiFiManager(DisplayManager& displayManager, WiFiManager& wifiManager, DebugManager& debugManager);
 	~DisplayWiFiManager();	
 
-	void 							printSignal ();
+	void 															printSignal ();
+									
+private:								
+								
+	DisplayManager*       											_displayManager;	
+	WiFiManager*          											_wifiManager;
+	DebugManager*         											_debugManager;
+											
+	bool 															_firstTimecaptivePortalCallback = true;
+											
+	void															printPortalHeaderInDisplay(String title);
+	void 															showEnteringSetup();
+	void 															showWiFiConect();
+										
+	void															printConnectedSignal(int x, int y, int barWidth, int margin, int barSignal);
+	void															printNoConnectedSignal(int x, int y, int barWidth, int margin);
+								
+	void 															startConfigPortalCallback ();
+	void 															captivePortalCallback (String ip);
+	void 															successConfigPortalCallback ();
+	void 															failedConfigPortalCallback (int connectionResult);
+	void 															connectingConfigPortalCallback ();
 	
-private:
-
-	DisplayManager*       			_displayManager;	
-	WiFiManager*          			_wifiManager;
-	DebugManager*         			_debugManager;
-			
-	bool 							_firstTimecaptivePortalCallback = true;
-			
-	void							printPortalHeaderInDisplay(String title);
-	void 							showEnteringSetup();
-	void 							showWiFiConect();
-		
-	void							printConnectedSignal(int x, int y, int barWidth, int margin, int barSignal);
-	void							printNoConnectedSignal(int x, int y, int barWidth, int margin);
-
-	void 							startConfigPortalCallback ();
-	void 							captivePortalCallback (String ip);
-	void 							successConfigPortalCallback ();
-	void 							failedConfigPortalCallback (int connectionResult);
-	void 							connectingConfigPortalCallback ();
-	
-	std::function<void(void)> 		_startConfigPortalCallback;
-	std::function<void(String)>		_captivePortalCallback;
-	std::function<void(void)> 		_successConfigPortalCallback;
-	std::function<void(int)>		_failedConfigPortalCallback;
-	std::function<void(void)> 		_connectingConfigPortalCallback;
+	WIFI_MANAGER_SET_START_CONFIG_PORTAL_CALLBACK_SIGNATURE 		_startConfigPortalCallback;
+	WIFI_MANAGER_SET_CAPTIVE_PORTAL_CALLBACK_SIGNATURE				_captivePortalCallback;
+	WIFI_MANAGER_SET_SUCCESS_CONFIG_PORTAL_CALLBACK_SIGNATURE 		_successConfigPortalCallback;
+	WIFI_MANAGER_SET_FAILED_CONFIG_PORTAL_CALLBACK_SIGNATURE		_failedConfigPortalCallback;
+	WIFI_MANAGER_SET_CONNECTING_CONFIG_PORTAL_CALLBACK_SIGNATURE 	_connectingConfigPortalCallback;
 	
 };
 
