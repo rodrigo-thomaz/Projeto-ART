cb1507a9d4c11346d4fa03893b1cc6b62debd2e6
rthomaz <rodrigothomaz@msn.com>
2017-11-26 09:18:54 -0200

    ok

---
+++ souces/ART.Domotica.Domain/Interfaces/IDeviceNTPDomain.cs
@@ -0,0 +1,17 @@
+﻿namespace ART.Domotica.Domain.Interfaces
+{
+    using System;
+    using System.Threading.Tasks;
+    using ART.Domotica.Repository.Entities;
+
+    public interface IDeviceNTPDomain
+    {
+        #region Methods       
+
+        Task<DeviceNTP> SetTimeZone(Guid deviceNTPId, byte timeZoneId);
+
+        Task<DeviceNTP> SetUpdateIntervalInMilliSecond(Guid deviceNTPId, int updateIntervalInMilliSecond);
+
+        #endregion Methods
+    }
+}
---
+++ souces/ART.Domotica.Domain/Services/DeviceNTPDomain.cs
@@ -0,0 +1,77 @@
+﻿namespace ART.Domotica.Domain.Services
+{
+    using System.Threading.Tasks;
+    using ART.Domotica.Domain.Interfaces;
+    using ART.Domotica.Repository.Interfaces;
+    using ART.Domotica.Repository.Entities;
+    using System;
+    using ART.Infra.CrossCutting.Domain;
+    using Autofac;
+    using ART.Domotica.Repository;
+    using ART.Domotica.Repository.Repositories;
+
+    public class DeviceNTPDomain : DomainBase, IDeviceNTPDomain
+    {
+        #region Fields
+
+        private readonly IDeviceNTPRepository _deviceNTPRepository;
+        private readonly ITimeZoneRepository _timeZoneRepository;
+
+        #endregion Fields
+
+        #region Constructors
+
+        public DeviceNTPDomain(IComponentContext componentContext)
+        {
+            var context = componentContext.Resolve<ARTDbContext>();
+                        
+            _deviceNTPRepository = new DeviceNTPRepository(context);
+            _timeZoneRepository = new TimeZoneRepository(context);
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        public async Task<DeviceNTP> SetTimeZone(Guid deviceNTPId, byte timeZoneId)
+        {
+            var entity = await _deviceNTPRepository.GetById(deviceNTPId);
+
+            if (entity == null)
+            {
+                throw new Exception("DeviceNTP not found");
+            }
+
+            var timeZone = await _timeZoneRepository.GetById(timeZoneId);
+
+            if (timeZone == null)
+            {
+                throw new Exception("Time Zone not found");
+            }
+
+            entity.TimeZoneId = timeZoneId;
+
+            await _deviceNTPRepository.Update(entity);
+
+            return entity;
+        }
+
+        public async Task<DeviceNTP> SetUpdateIntervalInMilliSecond(Guid deviceNTPId, int updateIntervalInMilliSecond)
+        {
+            var entity = await _deviceNTPRepository.GetById(deviceNTPId);
+
+            if (entity == null)
+            {
+                throw new Exception("DeviceNTP not found");
+            }
+
+            entity.UpdateIntervalInMilliSecond = updateIntervalInMilliSecond;
+
+            await _deviceNTPRepository.Update(entity);
+
+            return entity;
+        }
+
+        #endregion Methods
+    }
+}
