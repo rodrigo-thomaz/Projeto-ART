b6fbc4d6ed80591ea242bec4a96d73432dcf3d53
rthomaz <rodrigothomaz@msn.com>
2017-11-28 10:21:31 -0200

    ok

---
+++ souces/ART.Domotica.Repository/Interfaces/ISensorTriggerRepository.cs
@@ -0,0 +1,13 @@
+﻿using ART.Domotica.Repository.Entities;
+using ART.Infra.CrossCutting.Repository;
+using System;
+
+namespace ART.Domotica.Repository.Interfaces
+{
+    public interface ISensorTriggerRepository : IRepository<ARTDbContext, SensorTrigger, Guid>
+    {
+        #region Methods
+
+        #endregion Methods
+    }
+}
---
+++ souces/ART.Domotica.Repository/Repositories/SensorTriggerRepository.cs
@@ -0,0 +1,15 @@
+﻿using ART.Domotica.Repository.Entities;
+using ART.Domotica.Repository.Interfaces;
+using ART.Infra.CrossCutting.Repository;
+using System;
+
+namespace ART.Domotica.Repository.Repositories
+{
+    public class SensorTriggerRepository : RepositoryBase<ARTDbContext, SensorTrigger, Guid>, ISensorTriggerRepository
+    {
+        public SensorTriggerRepository(ARTDbContext context) : base(context)
+        {
+
+        }
+    }
+}
