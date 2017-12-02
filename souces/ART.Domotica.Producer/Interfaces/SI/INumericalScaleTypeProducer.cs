717c974d8b5c14069d4c9d6d0b53ee2798875ed2
rthomaz <rodrigothomaz@msn.com>
2017-12-02 12:33:48 -0200

    ok

---
+++ souces/ART.Domotica.Producer/Interfaces/SI/INumericalScaleTypeProducer.cs
@@ -0,0 +1,15 @@
+﻿namespace ART.Domotica.Producer.Interfaces.SI
+{
+    using System.Threading.Tasks;
+
+    using ART.Infra.CrossCutting.MQ.Contract;
+
+    public interface INumericalScaleTypeProducer
+    {
+        #region Methods
+
+        Task GetAll(AuthenticatedMessageContract message);
+
+        #endregion Methods
+    }
+}
