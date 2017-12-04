ebd6a59eec6ced012e1a053d29a1053e1d484ef1
rthomaz <rodrigothomaz@msn.com>
2017-12-04 13:17:07 -0200

    ok

---
+++ souces/ART.Domotica.Domain/Interfaces/IDeviceSernsorsDomain.cs
@@ -0,0 +1,15 @@
+﻿namespace ART.Domotica.Domain.Interfaces
+{
+    using System;
+    using System.Threading.Tasks;
+    using System.Collections.Generic;
+
+    public interface IDeviceSernsorsDomain
+    {
+        #region Methods
+
+        Task<List<DeviceSernsors>> GetAllByApplicationId(Guid applicationId);
+
+        #endregion Methods
+    }
+}
---
+++ souces/ART.Domotica.Domain/Interfaces/ISensorsInDeviceDomain.cs
@@ -0,0 +1,17 @@
+﻿namespace ART.Domotica.Domain.Interfaces
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using ART.Domotica.Repository.Entities;
+    using System.Collections.Generic;
+
+    public interface ISensorsInDeviceDomain
+    {
+        #region Methods
+
+        Task<List<SensorsInDevice>> GetAllByApplicationId(Guid applicationId);
+
+        #endregion Methods
+    }
+}
