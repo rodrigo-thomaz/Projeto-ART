03cee0a12644eb2bd25c7cd30f799fdb7e2de9c2
rthomaz <rodrigothomaz@msn.com>
2017-12-06 13:55:16 -0200

    ok

--- souces/ART.Domotica.UI.Web/src/js/services/hardware/sensorDatasheet/sensorDatasheetConstants.js
+++ souces/ART.Domotica.UI.Web/src/js/services/hardware/sensorDatasheet/sensorDatasheetConstants.js
@@ -1,6 +1,24 @@
 ﻿'use strict';
+app.constant('sensorTypeConstants', {
+    getAllApiUri: 'api/sensorType/getAll',
+    getAllCompletedTopic: 'SensorType.GetAllViewCompleted',
+    getAllCompletedEventName: 'sensorTypeService.GetAllCompleted',
+});
+
 app.constant('sensorDatasheetConstants', {
+    getAllApiUri: 'api/sensorDatasheet/getAll',
+    getAllCompletedTopic: 'SensorDatasheet.GetAllViewCompleted',
+    getAllCompletedEventName: 'sensorDatasheetService.GetAllCompleted',
+});
 
-    
+app.constant('sensorUnitMeasurementDefaultConstants', {
+    getAllApiUri: 'api/sensorUnitMeasurementDefault/getAll',
+    getAllCompletedTopic: 'SensorUnitMeasurementDefault.GetAllViewCompleted',
+    getAllCompletedEventName: 'sensorUnitMeasurementDefaultService.GetAllCompleted',
+});
 
+app.constant('sensorDatasheetUnitMeasurementScaleConstants', {
+    getAllApiUri: 'api/sensorDatasheetUnitMeasurementScale/getAll',
+    getAllCompletedTopic: 'SensorDatasheetUnitMeasurementScale.GetAllViewCompleted',
+    getAllCompletedEventName: 'sensorDatasheetUnitMeasurementScaleService.GetAllCompleted',
 });
