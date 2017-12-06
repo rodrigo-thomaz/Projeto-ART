e5a5f3f90a81d3a718b5a32768ccaba6e8b49829
rthomaz <rodrigothomaz@msn.com>
2017-12-06 14:28:16 -0200

    ok

--- souces/ART.Domotica.UI.Web/src/js/services/hardware/sensorDatasheet/sensorDatasheetMapper.js
+++ souces/ART.Domotica.UI.Web/src/js/services/hardware/sensorDatasheet/sensorDatasheetMapper.js
@@ -1,5 +1,6 @@
 ﻿'use strict';
-app.factory('sensorDatasheetMapper', ['$rootScope', function ($rootScope) {
+app.factory('sensorDatasheetMapper', ['$rootScope', 'sensorDatasheetContext', 'sensorTypeConstant', 'sensorDatasheetConstant', 'sensorUnitMeasurementDefaultConstant', 'sensorDatasheetUnitMeasurementScaleConstant',
+    function ($rootScope, sensorDatasheetContext, sensorTypeConstant, sensorDatasheetConstant, sensorUnitMeasurementDefaultConstant, sensorDatasheetUnitMeasurementScaleConstant) {
 
     var serviceFactory = {};    
 
@@ -11,7 +12,37 @@ app.factory('sensorDatasheetMapper', ['$rootScope', function ($rootScope) {
 
     // *** Events Subscriptions
 
-   
+    var onSensorTypeGetAllCompleted = function (event, data) {
+        sensorTypeGetAllCompletedSubscription();
+        sensorDatasheetContext.sensorTypeLoaded = true;
+    }   
+
+    var onSensorDatasheetGetAllCompleted = function (event, data) {
+        sensorDatasheetGetAllCompletedSubscription();
+        sensorDatasheetContext.sensorDatasheetLoaded = true;
+    }  
+
+    var onSensorUnitMeasurementDefaultGetAllCompleted = function (event, data) {
+        sensorUnitMeasurementDefaultGetAllCompletedSubscription();
+        sensorDatasheetContext.sensorUnitMeasurementDefaultLoaded = true;
+    }  
+
+    var onSensorDatasheetUnitMeasurementScaleGetAllCompleted = function (event, data) {
+        sensorDatasheetUnitMeasurementScaleGetAllCompletedSubscription();
+        sensorDatasheetContext.sensorDatasheetUnitMeasurementScaleLoaded = true;
+    }  
+
+    var sensorTypeGetAllCompletedSubscription = $rootScope.$on(sensorTypeConstant.getAllCompletedEventName, onSensorTypeGetAllCompleted);
+    var sensorDatasheetGetAllCompletedSubscription = $rootScope.$on(sensorDatasheetConstant.getAllCompletedEventName, onSensorDatasheetGetAllCompleted);
+    var sensorUnitMeasurementDefaultGetAllCompletedSubscription = $rootScope.$on(sensorUnitMeasurementDefaultConstant.getAllCompletedEventName, onSensorUnitMeasurementDefaultGetAllCompleted);
+    var sensorDatasheetUnitMeasurementScaleGetAllCompletedSubscription = $rootScope.$on(sensorDatasheetUnitMeasurementScaleConstant.getAllCompletedEventName, onSensorDatasheetUnitMeasurementScaleGetAllCompleted);
+    
+    $rootScope.$on('$destroy', function () {
+        sensorTypeGetAllCompletedSubscription();        
+        sensorDatasheetGetAllCompletedSubscription();        
+        sensorUnitMeasurementDefaultGetAllCompletedSubscription();        
+        sensorDatasheetUnitMeasurementScaleGetAllCompletedSubscription();        
+    });
 
     // *** Events Subscriptions
     
