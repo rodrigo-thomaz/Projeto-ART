7be04446d09b07dbd48bad9e3a10d9c105202b02
rthomaz <rodrigothomaz@msn.com>
2017-12-10 08:01:33 -0200

    ok

--- souces/ART.Domotica.UI.Web/src/js/services/hardware/sensor/sensorFinder.js
+++ souces/ART.Domotica.UI.Web/src/js/services/hardware/sensor/sensorFinder.js
@@ -12,34 +12,7 @@ app.factory('sensorFinder', ['$rootScope', 'sensorContext', function ($rootScope
                 return item;
             }
         }
-    }
-
-    var getSensorTriggerByKey = function (sensorTriggerId) {
-        for (var i = 0; i < context.sensorTrigger.length; i++) {
-            var item = context.sensorTrigger[i];
-            if (item.sensorTriggerId === sensorTriggerId) {
-                return item;
-            }
-        }
-    }
-
-    var getSensorUnitMeasurementScaleByKey = function (sensorUnitMeasurementScaleId) {
-        for (var i = 0; i < context.sensorUnitMeasurementScale.length; i++) {
-            var item = context.sensorUnitMeasurementScale[i];
-            if (item.sensorUnitMeasurementScaleId === sensorUnitMeasurementScaleId) {
-                return item;
-            }
-        }
-    }
-
-    var getSensorTempDSFamilyByKey = function (sensorTempDSFamilyId) {
-        for (var i = 0; i < context.sensorTempDSFamily.length; i++) {
-            var item = context.sensorTempDSFamily[i];
-            if (item.sensorTempDSFamilyId === sensorTempDSFamilyId) {
-                return item;
-            }
-        }
-    }
+    }   
 
     var getSensorTempDSFamilyResolutionByKey = function (sensorTempDSFamilyResolutionId) {
         for (var i = 0; i < context.sensorTempDSFamilyResolution.length; i++) {
@@ -53,9 +26,6 @@ app.factory('sensorFinder', ['$rootScope', 'sensorContext', function ($rootScope
     // *** Public Methods ***
 
     serviceFactory.getSensorByKey = getSensorByKey;
-    serviceFactory.getSensorTriggerByKey = getSensorTriggerByKey;
-    serviceFactory.getSensorUnitMeasurementScaleByKey = getSensorUnitMeasurementScaleByKey;
-    serviceFactory.getSensorTempDSFamilyByKey = getSensorTempDSFamilyByKey;
     serviceFactory.getSensorTempDSFamilyResolutionByKey = getSensorTempDSFamilyResolutionByKey;
 
     return serviceFactory;
