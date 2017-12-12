b0bf81066a8581134492250928ab9274e86c0eea
rthomaz <rodrigothomaz@msn.com>
2017-12-12 12:08:57 -0200

    ok

--- souces/ART.Domotica.UI.Web/src/js/controllers/hardware/sensor/sensorUnitMeasurementScaleController.js
+++ souces/ART.Domotica.UI.Web/src/js/controllers/hardware/sensor/sensorUnitMeasurementScaleController.js
@@ -1,41 +1,63 @@
-﻿app.controller('sensorUnitMeasurementScaleController', ['$scope', '$rootScope', '$timeout', '$log', 'toaster', 'unitMeasurementConverter', 'sensorUnitMeasurementScaleService', function ($scope, $rootScope, $timeout, $log, toaster, unitMeasurementConverter, sensorUnitMeasurementScaleService) {
+﻿app.controller('sensorUnitMeasurementScaleController', ['$scope', '$rootScope', '$timeout', '$log', 'toaster', 'unitMeasurementConverter', 'sensorUnitMeasurementScaleService', 'siContext',
+    function ($scope, $rootScope, $timeout, $log, toaster, unitMeasurementConverter, sensorUnitMeasurementScaleService, siContext) {
 
-    $scope.sensorUnitMeasurementScale = null;    
+        $scope.sensorUnitMeasurementScale = null;
 
-    $scope.init = function (sensorUnitMeasurementScale) {
+        $scope.init = function (sensorUnitMeasurementScale) {
 
-        $scope.sensorUnitMeasurementScale = sensorUnitMeasurementScale;
-        
-        //clearOnSetValueCompleted = $rootScope.$on('sensorUnitMeasurementScaleService_SetValueCompleted_Id_' + sensor.sensorUnitMeasurementScale.id, onSetValueCompleted);
-    };
+            $scope.sensorUnitMeasurementScale = sensorUnitMeasurementScale;
 
-    //var clearOnSetValueCompleted = null;
+            // Time Zone
+            if (siContext.unitMeasurementScaleLoaded)
+                setSelectedSensorUnitMeasurementScale();
+            else {
+                var unitMeasurementScaleLoadedWatch = siContext.$watch('unitMeasurementScaleLoaded', function (newValue) {
+                    if (newValue) {
+                        unitMeasurementScaleLoadedWatch();
+                        setSelectedSensorUnitMeasurementScale();
+                    }
+                })
+            }
 
-    $scope.$on('$destroy', function () {
-        //clearOnSetValueCompleted();
-    });
+            //clearOnSetValueCompleted = $rootScope.$on('sensorUnitMeasurementScaleService_SetValueCompleted_Id_' + sensor.sensorUnitMeasurementScale.id, onSetValueCompleted);
+        };
 
-    //$scope.changeValue = function (position, value) {
-    //    if (!initialized || value === undefined) return;
-    //    var valueConverted = unitMeasurementConverter.convertToCelsius($scope.sensor.unitMeasurementId, value);
-    //    sensorUnitMeasurementScaleService.setValue($scope.sensor.sensorUnitMeasurementScale.id, valueConverted, position);
-    //};
+        $scope.sensorUnitMeasurementScaleView = {
+            availableSensorUnitMeasurementScale: siContext.unitMeasurementScale,
+            selectedSensorUnitMeasurementScale: {},
+        };
 
-    //$scope.$watch('sensor.sensorUnitMeasurementScale.maxConverted', function (newValue, oldValue) {
-    //    $scope.maxView = $scope.sensor.sensorUnitMeasurementScale.maxConverted;
-    //});
+        var setSelectedSensorUnitMeasurementScale = function () {
+            $scope.sensorUnitMeasurementScaleView.selectedSensorUnitMeasurementScale = $scope.sensorUnitMeasurementScale.unitMeasurementScale();
+        };
 
-    //$scope.$watch('sensor.sensorUnitMeasurementScale.minConverted', function (newValue, oldValue) {
-    //    $scope.minView = $scope.sensor.sensorUnitMeasurementScale.minConverted
-    //});
+        //var clearOnSetValueCompleted = null;
 
-    //var onSetValueCompleted = function (event, data) {
-    //    if (data.position === 'Max') {
-    //        toaster.pop('success', 'Sucesso', 'Limite alto do gráfico alterado');
-    //    }
-    //    else if (data.position === 'Min') {
-    //        toaster.pop('success', 'Sucesso', 'Limite baixo do gráfico alterado');
-    //    }
-    //};
+        $scope.$on('$destroy', function () {
+            //clearOnSetValueCompleted();
+        });
 
-}]);
+        //$scope.changeValue = function (position, value) {
+        //    if (!initialized || value === undefined) return;
+        //    var valueConverted = unitMeasurementConverter.convertToCelsius($scope.sensor.unitMeasurementId, value);
+        //    sensorUnitMeasurementScaleService.setValue($scope.sensor.sensorUnitMeasurementScale.id, valueConverted, position);
+        //};
+
+        //$scope.$watch('sensor.sensorUnitMeasurementScale.maxConverted', function (newValue, oldValue) {
+        //    $scope.maxView = $scope.sensor.sensorUnitMeasurementScale.maxConverted;
+        //});
+
+        //$scope.$watch('sensor.sensorUnitMeasurementScale.minConverted', function (newValue, oldValue) {
+        //    $scope.minView = $scope.sensor.sensorUnitMeasurementScale.minConverted
+        //});
+
+        //var onSetValueCompleted = function (event, data) {
+        //    if (data.position === 'Max') {
+        //        toaster.pop('success', 'Sucesso', 'Limite alto do gráfico alterado');
+        //    }
+        //    else if (data.position === 'Min') {
+        //        toaster.pop('success', 'Sucesso', 'Limite baixo do gráfico alterado');
+        //    }
+        //};
+
+    }]);
