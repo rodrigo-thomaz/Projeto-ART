30a5b1397668f090b089cd468c317c6001770ac5
rthomaz <rodrigothomaz@msn.com>
2017-12-13 07:48:16 -0200

    ok

--- souces/ART.Domotica.UI.Web/src/js/services/locale/countryFinder.js
+++ souces/ART.Domotica.UI.Web/src/js/services/locale/countryFinder.js
@@ -1,46 +1,47 @@
 ﻿'use strict';
-app.factory('countryFinder', ['$rootScope', 'localeContext', 'siContext', 'numericalScaleTypeFinder', function ($rootScope, localeContext, siContext, numericalScaleTypeFinder) {
+app.factory('countryFinder', ['$rootScope', 'localeContext', 'siContext', 'numericalScaleTypeFinder',
+    function ($rootScope, localeContext, siContext, numericalScaleTypeFinder) {
 
-    var context = localeContext;
+        var context = localeContext;
 
-    var serviceFactory = {}; 
+        var serviceFactory = {};
 
-    var getByKey = function (countryId) {
-        for (var i = 0; i < context.country.length; i++) {
-            var item = context.country[i];
-            if (item.countryId === countryId) {
-                return item;
+        var getByKey = function (countryId) {
+            for (var i = 0; i < context.country.length; i++) {
+                var item = context.country[i];
+                if (item.countryId === countryId) {
+                    return item;
+                }
             }
         }
-    }  
 
-    var getByContinentKey = function (continentId) {
-        var result = [];
-        for (var i = 0; i < context.country.length; i++) {
-            if (context.country[i].continentId === continentId) {
-                result.push(context.country[i]);
+        var getByContinentKey = function (continentId) {
+            var result = [];
+            for (var i = 0; i < context.country.length; i++) {
+                if (context.country[i].continentId === continentId) {
+                    result.push(context.country[i]);
+                }
             }
+            return result;
         }
-        return result;
-    }  
 
-    var getByNumericalScaleTypeKey = function (numericalScaleTypeId) {
-        var result = [];
-        for (var i = 0; i < siContext.numericalScaleTypeCountry.length; i++) {
-            if (siContext.numericalScaleTypeCountry[i].numericalScaleTypeId === numericalScaleTypeId) {
-                var country = getByKey(siContext.numericalScaleTypeCountry[i].countryId);
-                result.push(country);
+        var getByNumericalScaleTypeKey = function (numericalScaleTypeId) {
+            var result = [];
+            for (var i = 0; i < siContext.numericalScaleTypeCountry.length; i++) {
+                if (siContext.numericalScaleTypeCountry[i].numericalScaleTypeId === numericalScaleTypeId) {
+                    var country = getByKey(siContext.numericalScaleTypeCountry[i].countryId);
+                    result.push(country);
+                }
             }
+            return result;
         }
-        return result;
-    } 
 
-    // *** Public Methods ***
+        // *** Public Methods ***
 
-    serviceFactory.getByKey = getByKey;
-    serviceFactory.getByContinentKey = getByContinentKey;
-    serviceFactory.getByNumericalScaleTypeKey = getByNumericalScaleTypeKey;
+        serviceFactory.getByKey = getByKey;
+        serviceFactory.getByContinentKey = getByContinentKey;
+        serviceFactory.getByNumericalScaleTypeKey = getByNumericalScaleTypeKey;
 
-    return serviceFactory;
+        return serviceFactory;
 
-}]);
+    }]);
--- souces/ART.Domotica.UI.Web/src/js/services/si/numericalScaleTypeCountryFinder.js
+++ souces/ART.Domotica.UI.Web/src/js/services/si/numericalScaleTypeCountryFinder.js
@@ -1,45 +1,46 @@
 ﻿'use strict';
-app.factory('numericalScaleTypeCountryFinder', ['$rootScope', 'siContext', function ($rootScope, siContext) {
+app.factory('numericalScaleTypeCountryFinder', ['$rootScope', 'siContext',
+    function ($rootScope, siContext) {
 
-    var context = siContext;
+        var context = siContext;
 
-    var serviceFactory = {};    
+        var serviceFactory = {};
 
-    var getByKey = function (numericalScaleTypeId, countryId) {
-        for (var i = 0; i < context.numericalScaleTypeCountry.length; i++) {
-            var item = context.numericalScaleTypeCountry[i];
-            if (item.numericalScaleTypeId === numericalScaleTypeId && item.countryId === countryId) {
-                return item;
+        var getByKey = function (numericalScaleTypeId, countryId) {
+            for (var i = 0; i < context.numericalScaleTypeCountry.length; i++) {
+                var item = context.numericalScaleTypeCountry[i];
+                if (item.numericalScaleTypeId === numericalScaleTypeId && item.countryId === countryId) {
+                    return item;
+                }
             }
         }
-    }
 
-    var getByNumericalScaleTypeKey = function (numericalScaleTypeId) {
-        var result = [];
-        for (var i = 0; i < context.numericalScaleTypeCountry.length; i++) {
-            if (context.numericalScaleTypeCountry[i].numericalScaleTypeId === numericalScaleTypeId) {
-                result.push(context.numericalScaleTypeCountry[i]);
+        var getByNumericalScaleTypeKey = function (numericalScaleTypeId) {
+            var result = [];
+            for (var i = 0; i < context.numericalScaleTypeCountry.length; i++) {
+                if (context.numericalScaleTypeCountry[i].numericalScaleTypeId === numericalScaleTypeId) {
+                    result.push(context.numericalScaleTypeCountry[i]);
+                }
             }
+            return result;
         }
-        return result;
-    } 
 
-    var getByCountryKey = function (countryId) {
-        var result = [];
-        for (var i = 0; i < context.numericalScaleTypeCountry.length; i++) {
-            if (context.numericalScaleTypeCountry[i].countryId === countryId) {
-                result.push(context.numericalScaleTypeCountry[i]);
+        var getByCountryKey = function (countryId) {
+            var result = [];
+            for (var i = 0; i < context.numericalScaleTypeCountry.length; i++) {
+                if (context.numericalScaleTypeCountry[i].countryId === countryId) {
+                    result.push(context.numericalScaleTypeCountry[i]);
+                }
             }
+            return result;
         }
-        return result;
-    }    
 
-    // *** Public Methods ***
+        // *** Public Methods ***
 
-    serviceFactory.getByKey = getByKey;
-    serviceFactory.getByNumericalScaleTypeKey = getByNumericalScaleTypeKey;
-    serviceFactory.getByCountryKey = getByCountryKey;
+        serviceFactory.getByKey = getByKey;
+        serviceFactory.getByNumericalScaleTypeKey = getByNumericalScaleTypeKey;
+        serviceFactory.getByCountryKey = getByCountryKey;
 
-    return serviceFactory;
+        return serviceFactory;
 
-}]);
+    }]);
