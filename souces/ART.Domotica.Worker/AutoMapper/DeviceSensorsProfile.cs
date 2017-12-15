4e3e95afe2f1e25f08e10df849542e0752f95cf6
rthomaz <rodrigothomaz@msn.com>
2017-12-15 20:00:13 -0200

    ok

--- souces/ART.Domotica.Worker/AutoMapper/DeviceSensorsProfile.cs
+++ souces/ART.Domotica.Worker/AutoMapper/DeviceSensorsProfile.cs
@@ -4,6 +4,7 @@
     using ART.Domotica.Repository.Entities;
 
     using global::AutoMapper;
+    using System.Linq;
 
     public class DeviceSensorsProfile : Profile
     {
@@ -14,7 +15,7 @@
             CreateMap<DeviceSensors, DeviceSensorsGetModel>()
                 .ForMember(vm => vm.DeviceSensorsId, m => m.MapFrom(x => x.Id))
                 .ForMember(vm => vm.PublishIntervalInSeconds, m => m.MapFrom(x => x.PublishIntervalInSeconds))
-                .ForMember(vm => vm.SensorInDevice, m => m.MapFrom(x => x.SensorInDevice));
+                .ForMember(vm => vm.SensorInDevice, m => m.MapFrom(x => x.SensorInDevice.OrderBy(y => y.Ordination)));
 
             CreateMap<DeviceSensors, DeviceSensorsSetPublishIntervalInSecondsModel>()
                 .ForMember(vm => vm.DeviceSensorsId, m => m.MapFrom(x => x.Id))
