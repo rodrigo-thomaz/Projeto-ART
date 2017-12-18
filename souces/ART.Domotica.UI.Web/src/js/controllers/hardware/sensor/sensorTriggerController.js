072f07ee9f83b2afcd81ee6efffb155cfe7e6e09
rthomaz <rodrigothomaz@msn.com>
2017-12-18 11:16:13 -0200

    ok

--- souces/ART.Domotica.UI.Web/src/js/controllers/hardware/sensor/sensorTriggerController.js
+++ souces/ART.Domotica.UI.Web/src/js/controllers/hardware/sensor/sensorTriggerController.js
@@ -1,1 +1,16 @@
-﻿
+﻿'use strict';
+app.controller('sensorTriggerController', ['$scope', '$rootScope', '$timeout', '$log', 'toaster', 'sensorContext', 'sensorTriggerService', 'sensorTriggerFinder', 'sensorTriggerConstant',
+    function ($scope, $rootScope, $timeout, $log, toaster, sensorContext, sensorTriggerService, sensorTriggerFinder, sensorTriggerConstant) {
+
+        $scope.sensorTriggers = null;
+
+        $scope.init = function (sensorTriggers) {
+            $scope.sensorTriggers = sensorTriggers;
+        };        
+
+        $scope.$on('$destroy', function () {
+            
+        });        
+
+    }]);
+
