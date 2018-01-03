56895819dc39aea20a34a5a7e48489b1e16e42e4
rthomaz <rodrigothomaz@msn.com>
2018-01-03 16:54:04 -0200

    ok

--- souces/ART.Domotica.UI.Web/src/js/controllers/device/deviceDebugController.js
+++ souces/ART.Domotica.UI.Web/src/js/controllers/device/deviceDebugController.js
@@ -4,30 +4,42 @@ app.controller('deviceDebugController', ['$scope', '$rootScope', '$timeout', '$l
 
         $scope.deviceDebug = null;
 
+        $scope.activeView = null;
+
         $scope.init = function (deviceDebug) {
 
             $scope.deviceDebug = deviceDebug;
 
             $scope.activeView = deviceDebug.active;
 
+            initializeActiveViewWatch();
+
             clearOnSetActiveCompleted = $rootScope.$on(deviceDebugConstant.setActiveCompletedEventName + $scope.deviceDebug.deviceDebugId, onSetActiveCompleted);
         }
+
+        var activeViewWatch = null;
+
+        var initializeActiveViewWatch = function () {
+            activeViewWatch = $scope.$watch('activeView', function (newValue, oldValue) {
+                if (newValue === oldValue) return;
+                deviceDebugService.setActive(
+                    $scope.deviceDebug.deviceDebugId, 
+                    $scope.deviceDebug.deviceDatasheetId, 
+                    newValue);
+            });
+        };
 
         var clearOnSetActiveCompleted = null;
 
         $scope.$on('$destroy', function () {
             clearOnSetActiveCompleted();
+            activeViewWatch();
         });
 
         var onSetActiveCompleted = function (event, data) {
             $scope.activeView = $scope.deviceDebug.active;
             $scope.$apply();
             toaster.pop('success', 'Sucesso', 'Debug active alterado');
-        };
-
-        $scope.changeActive = function () {
-            if (!$scope.deviceDebug || !$scope.activeView) return;
-            deviceDebugService.setActive($scope.deviceDebug.deviceDebugId, $scope.deviceDebug.deviceDatasheetId, $scope.activeView);
         };
 
     }]);
