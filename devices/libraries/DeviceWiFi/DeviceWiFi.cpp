720fadf7eda59a213038b180969cee4865fff35f
rthomaz <rodrigothomaz@msn.com>
2018-02-07 09:50:02 -0200

    ok

--- devices/libraries/DeviceWiFi/DeviceWiFi.cpp
+++ devices/libraries/DeviceWiFi/DeviceWiFi.cpp
@@ -1036,12 +1036,17 @@ namespace ART
 	}
 
 	bool DeviceWiFi::onDeviceMQSubscription(char* topicKey, char* json)
-	{
-		/*if (strcmp(topicKey, DEVICE_WIFI_SET_HOST_NAME_TOPIC_SUB) == 0) {
+	{/*
+		if (strcmp(topicKey, DEVICE_WIFI_SET_HOST_NAME_TOPIC_SUB) == 0) {
 			setHostName(json);
+			return true;
 		}
 		else if (strcmp(topicKey, DEVICE_WIFI_SET_PUBLISH_INTERVAL_IN_MILLI_SECONDS_TOPIC_SUB) == 0) {
 			setPublishIntervalInMilliSeconds(json);
+			return true;
+		}
+		else {
+			return false;
 		}*/
 	}
 }
