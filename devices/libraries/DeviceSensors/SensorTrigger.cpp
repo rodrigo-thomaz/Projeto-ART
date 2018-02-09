48089d6ebdb5049d221ba3bf3aacf2e06478c614
rthomaz <rodrigothomaz@msn.com>
2018-02-09 19:56:44 -0200

    ok

--- devices/libraries/DeviceSensors/SensorTrigger.cpp
+++ devices/libraries/DeviceSensors/SensorTrigger.cpp
@@ -8,14 +8,16 @@ namespace ART
 		Serial.println(F("[SensorTrigger constructor]"));
 
 		_sensor = sensor;	
-
+				
 		char* sensorTriggerId = strdup(jsonObject["sensorTriggerId"]);
 		_sensorTriggerId = new char(sizeof(strlen(sensorTriggerId)));
 		_sensorTriggerId = sensorTriggerId;
-
-		/*const char* sensorTriggerId = jsonObject["sensorTriggerId"];
-		_sensorTriggerId = new char(sizeof(strlen(sensorTriggerId)));
-		strcpy(_sensorTriggerId, sensorTriggerId);*/
+		
+		/*
+		const char* sensorTriggerId = jsonObject["sensorTriggerId"];
+		_sensorTriggerId = new char(strlen(sensorTriggerId) + 1);
+		strcpy(_sensorTriggerId, sensorTriggerId);
+		*/
 
 		_triggerOn = bool(jsonObject["triggerOn"]);
 		_buzzerOn = bool(jsonObject["buzzerOn"]);
