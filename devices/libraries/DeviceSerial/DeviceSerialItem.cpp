99172edd471a8ed5451a1c1bd0bb5369685fe3df
rthomaz <rodrigothomaz@msn.com>
2018-02-10 21:48:12 -0200

    ok

--- devices/libraries/DeviceSerial/DeviceSerialItem.cpp
+++ devices/libraries/DeviceSerial/DeviceSerialItem.cpp
@@ -1,15 +1,53 @@
 #include "DeviceSerialItem.h"
-#include "../ESPDevice/ESPDevice.h"
 
 namespace ART
 {
-	DeviceSerialItem::DeviceSerialItem()
+	DeviceSerialItem::DeviceSerialItem(JsonObject& jsonObject)
 	{
-		
+		Serial.println(F("[DeviceSerialItem:DeviceSerialItem] constructor"));
+
+		_hasRX = int(jsonObject["hasRX"]);
+		_hasTX = int(jsonObject["hasTX"]);
+
+		_allowPinSwapRX = int(jsonObject["allowPinSwapRX"]);
+		_allowPinSwapTX = int(jsonObject["allowPinSwapTX"]);
+
+		_pinRX = int(jsonObject["pinRX"]);
+		_pinTX = int(jsonObject["pinTX"]);
 	}
 
 	DeviceSerialItem::~DeviceSerialItem()
 	{
+		Serial.println(F("[DeviceSerialItem:DeviceSerialItem] destructor"));
+	}
 
+	void DeviceSerialItem::setHasRX(bool value)
+	{
+		_hasRX = value;
+	}
+
+	void DeviceSerialItem::setHasTX(bool value)
+	{
+		_hasTX = value;
+	}
+
+	void DeviceSerialItem::setAllowPinSwapRX(bool value)
+	{
+		_allowPinSwapRX = value;
+	}
+
+	void DeviceSerialItem::setAllowPinSwapTX(bool value)
+	{
+		_allowPinSwapTX = value;
+	}
+
+	void DeviceSerialItem::setPinRX(short value)
+	{
+		_pinRX = value;
+	}
+
+	void DeviceSerialItem::setPinTX(short value)
+	{
+		_pinTX = value;
 	}
 }
--- devices/libraries/DeviceSerial/DeviceSerialItem.h
+++ devices/libraries/DeviceSerial/DeviceSerialItem.h
@@ -1,6 +1,8 @@
 #ifndef DeviceSerialItem_h
 #define DeviceSerialItem_h
 
+#include "../ArduinoJson/ArduinoJson.h"
+
 namespace ART
 {
 	class DeviceSerialItem
@@ -8,13 +10,30 @@ namespace ART
 
 	public:
 
-		DeviceSerialItem();
+		DeviceSerialItem(JsonObject& jsonObject);
 		~DeviceSerialItem();
 
 	private:
 
+		bool								_hasRX;
+		bool								_hasTX;
 
+		bool								_allowPinSwapRX;
+		bool								_allowPinSwapTX;
 
+		short								_pinRX;
+		short								_pinTX;
+
+		void								setHasRX(bool value);
+		void								setHasTX(bool value);
+
+		void								setAllowPinSwapRX(bool value);
+		void								setAllowPinSwapTX(bool value);
+
+		void								setPinRX(short value);
+		void								setPinTX(short value);
+
+		friend class						DeviceSerial;
 	};
 }
 
